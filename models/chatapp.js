ad34dd7b4193ce41bc5fd9a762b787c6402774c3
kurukshetra <kurukshetracodingschool.com>
2025-07-24 21:12:47 +0530

    db connection

--- models/chatapp.js
+++ models/chatapp.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://0.0.0.0/chataapp').then(()=>{
+mongoose.connect('mongodb://0.0.0.0/chatappRoom').then(()=>{
     console.log("mongodb Connected✅");
 })
 
 const chatappSchema = mongoose.Schema({
-    content:String,
+    username:String,
+    msg:String,
+    roomName:String,
     timestamp:{
         type:Date,
         default:Date.now
     }
 })
-module.exports =mongoose.model('Chatapp',chatappSchema);
+module.exports =mongoose.model('Chats',chatappSchema);
--- socketio.js
+++ socketio.js
@@ -1,17 +1,43 @@
-const io =require('socket.io')();
+const io = require('socket.io')();
 const Message = require('./models/chatapp')
 const socketapi = {
-    io:io
+    io: io
 }
 // lOgic
-io.on('connection',async function(socket){
+io.on('connection', async function (socket) {
     console.log("new user conneted");
-    socket.on("sony",(msg)=>{
-        console.log(msg)
-        socket.broadcast.to(msg.roomName).emit('incomingMessage',msg)
-    })
-    socket.on('joinRoom',roomName=>{
+
+    socket.on('joinRoom', async (roomName) => {
         socket.join(roomName);
+        // sends past 100 msg 
+        const messages = await Message.find({ roomName }).sort({ timestamp: 1 }).limit(100);
+        messages.forEach((msg) => {
+            1
+            socket.emit('incomingMessage', msg);// send the history to joined user
+        });
+    });
+
+    socket.on('deleteMessage', async (msgId) => {
+        try {
+            await Message.findByIdAndDelete(msgId);
+            console.log('messagedelete',msgId);
+            
+            // aall user show that
+            socket.broadcast.emit("messageDeleted", msgId)
+
+        } catch (error) {
+            console.error("error of Deleteing Msg", error);
+        }
+    })
+
+
+    socket.on("sony", async (msg) => {
+        // console.log(msg)
+        // save the messages
+        const newmsg = new Message(msg);
+        await newmsg.save();
+
+        socket.broadcast.to(msg.roomName).emit('incomingMessage', msg)
     })
 })
 module.exports = socketapi;
